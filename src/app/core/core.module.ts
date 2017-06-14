e6b3f552a8fabe3eb66f915e67acf1b213e13218
Spencer Curry <spencer@trifectaecosystems.com>
2017-06-14 08:30:13 -0400

    added a few todo items and questions.  committing to make a clean slate for my work today

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
-MdButtonModule,
-MdCardModule,
-MdDialogModule,
-MdIconModule,
-MdInputModule,
-MdMenuModule,
-MdProgressSpinnerModule,
-MdSidenavModule,
-MdSlideToggleModule,
-MdSnackBarModule,
-MdToolbarModule,
-MdTooltipModule
+  MdButtonModule,
+  MdCardModule,
+  MdDialogModule,
+  MdIconModule,
+  MdInputModule,
+  MdMenuModule,
+  MdProgressSpinnerModule,
+  MdSidenavModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdToolbarModule,
+  MdTooltipModule
 } from '@angular/material';
 
 
-import {AppModule} from "../app.module";
+import {AppModule} from '../app.module';
 
 @NgModule({
   declarations: [
